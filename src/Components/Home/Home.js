847fc31cdfa7724909ff1230a9b3210afa07f5c9
Joseph_Tully <tully4school@gmail.com>
2020-01-28 23:14:13 -0500

    removed an error

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -22,13 +22,13 @@ function Home(props) {
             <div className="cards my-5">
                 <div className="row">
                     <div className="col-md-4">
-                        <Link className="text-dark" Card imgLink={Criminal} cardLabel="Criminal Defense" />
+                        <LinkCard imgLink={Criminal} cardLabel="Criminal Defense" />
                     </div>
                     <div className="col-md-4">
-                        <Link className="text-dark" Card imgLink={Traffic} cardLabel="Traffic Defense" />
+                        <LinkCard imgLink={Traffic} cardLabel="Traffic Defense" />
                     </div>
                     <div className="col-md-4">
-                        <Link className="text-dark" Card imgLink={Civil} cardLabel="Civil Litigation" />
+                        <LinkCard imgLink={Civil} cardLabel="Civil Litigation" />
                     </div>
                 </div>
             </div>
@@ -36,7 +36,7 @@ function Home(props) {
             <div className="metastuff">
                 <div className="row">
                     <div className="col-lg-12">
-                        <div className="map float-right px-5 py-2">
+                        <div className="map float-right px-5 py-2 d-none d-lg-block">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3109.21634867391!2d-77.04777534824889!3d38.80459577948478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b283e250fa1f%3A0x3e790f2c6902346a!2sThe%20Brown%20Firm%20PLLC!5e0!3m2!1sen!2sus!4v1580270466922!5m2!1sen!2sus" width="400" height="400" frameborder="0" allowfullscreen=""></iframe>
                         </div>
                         <h3>
