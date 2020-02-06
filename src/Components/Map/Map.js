9470d50ed57c94383627bef063ac729d2e428ac7
Joseph_Tully <tully4school@gmail.com>
2020-02-06 12:48:42 -0500

    added box shadow to the map

---
+++ src/Components/Map/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Map.css'
+
+function Map() {
+    return (
+        <div className="map float-right px-5 py-2 d-none d-lg-block">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3109.21634867391!2d-77.04777534824889!3d38.80459577948478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b283e250fa1f%3A0x3e790f2c6902346a!2sThe%20Brown%20Firm%20PLLC!5e0!3m2!1sen!2sus!4v1580270466922!5m2!1sen!2sus" width="400" height="400" frameborder="0" allowfullscreen=""></iframe>
+        </div>
+    )
+}
+export default Map
